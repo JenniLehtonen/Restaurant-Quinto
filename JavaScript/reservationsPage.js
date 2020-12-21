c2d6039f4474b92f7ae374dd3c013289f38e5553
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2020-12-21 17:29:08 +0200

    User input validation

--- JavaScript/reservationsPage.js
+++ JavaScript/reservationsPage.js
@@ -13,10 +13,26 @@ $(function(){
     }
   });
 });
+
+var today = new Date();  
+var day = today.getDate();
+var month = today.getMonth()+1;
+var year = today.getFullYear();
+ if (day < 10){
+        day = '0'+day
+    } 
+    if (month < 10){
+        month = '0'+month
+    } 
+
+today = year+'-'+month+'-'+day;
+document.getElementById("date").setAttribute("min", today); //minimum day in the calendar is today
+
   function checkInput(name, phonenumber, date){
     this.name = name;
     this.phonenumber = phonenumber;
     this.date = date;
+
     if(name ==""){
       nameElement.classList.add("inputInvalid");
       document.getElementById("nameError").style.display="block";
@@ -31,6 +47,9 @@ $(function(){
       phoneElement.classList.remove("inputInvalid");
       document.getElementById("phoneError").style.display="none";
       document.getElementById("completed").style.display="none";
+    } if (phonenumber.length < 4){
+      phoneElement.classList.add("inputInvalid");
+      document.getElementById("phoneError").style.display="block";
     } if (date ==""){
       dateElement.classList.add("inputInvalid");
       document.getElementById("dateError").style.display="block";
@@ -38,7 +57,7 @@ $(function(){
       dateElement.classList.remove("inputInvalid");
       document.getElementById("dateError").style.display="none";
       document.getElementById("completed").style.display="none";
-    } if(name != "" && phonenumber != "" && date != "") { //if everything is okay show "completed" div
+    } if(name != "" && phonenumber != "" && date != "" && phonenumber.length >= 4) { //if everything is okay show "completed" div
       document.getElementById("completed").style.display="block";
     }
   }
